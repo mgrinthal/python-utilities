4d45b46d059812675470a67023f583ea01b81a0c
mgrinthal <mgrinthal@users.noreply.github.com>
2017-05-16 19:53:42 -0400

    Switch to PRAW

--- reddit_poller.py
+++ reddit_poller.py
@@ -1,19 +1,14 @@
-import email_lib, requests, requests.auth, configparser
+import email_lib, requests, requests.auth, configparser, praw
 
 configParser = configparser.RawConfigParser()
 configPath = './reddit-config.txt'
 configParser.read(configPath)
 
 username = configParser.get('reddit-config', 'username')
-
-client_auth = requests.auth.HTTPBasicAuth(configParser.get('reddit-config', 'client-id'), configParser.get('reddit-config', 'secret'))
-post_data = {"grant_type": "password", "username": username, "password": configParser.get('reddit-config', 'password')}
 ua_string = 'RedditPoller/0.1 by' + username
-headers = {"User-Agent": ua_string}
-response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
-response = response.json()
 
-authorization = 'bearer ' + response['access_token']
+reddit = praw.Reddit(client_id=configParser.get('reddit-config', 'client-id'),
+                     client_secret=configParser.get('reddit-config', 'secret'),
+                     user_agent=ua_string)
 
-headers = {"Authorization": authorization, "User-Agent": ua_string}
-# Can now make requests with the OAuth token
+print(reddit.read_only)

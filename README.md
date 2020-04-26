7ac0b40cf156bdbc2d4131339066f00846978aa2
Michael Grinthal <michael.grinthal@dashlane.com>
2020-04-26 12:46:43 -0400

    Upgrade to Python 3

---
+++ README.md
@@ -0,0 +1,13 @@
+Works with Python 3
+
+add an email-config.txt file with address field
+[email-config]
+address=
+
+add a reddit-config.txt file with username, client id, and secret
+[reddit-config]
+username=
+client-id=
+secret=
+
+Customize keywords in reddit_poller.py as necessary
--- email_lib.py
+++ email_lib.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 
-import sys, smtplib, getpass, configparser
+import sys
+import smtplib
+import getpass
+import configparser
 
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -10,43 +13,45 @@ configPath = './email-config.txt'
 configParser.read(configPath)
 
 emailAddress = configParser.get('email-config', 'address')
-password = configParser.get('email-config', 'password')
+password = getpass.getpass()
+
 
 def send(subject, body):
-  if not subject and not body:
-    print('Error: Email message must have a subject line or body')
-    sys.exit()
+    if not subject and not body:
+        print('Error: Email message must have a subject line or body')
+        sys.exit()
 
-  smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
-  connectionRes = smtpObj.ehlo()
+    smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+    connectionRes = smtpObj.ehlo()
 
-  if connectionRes[0] == 250:
-    # Successful connection, enable encryption
-    smtpObj.starttls()
-    try:
-      loginRes = smtpObj.login(emailAddress, password)
-    except smtplib.SMTPException as err:
-      print('Error occured: ' + str(err[0]) + ' ' + str(err[1]))
-      smtpObj.quit()
-      sys.exit()
-    
-    # emailRes = smtpObj.sendmail(emailAddress, emailAddress,
-    #                             'Subject: ' + str(subject) + '\n' + str(body))
+    if connectionRes[0] == 250:
+        # Successful connection, enable encryption
+        smtpObj.starttls()
+        try:
+            loginRes = smtpObj.login(emailAddress, password)
+        except smtplib.SMTPException as err:
+            print('Error occured: ' + str(err[0]) + ' ' + str(err[1]))
+            smtpObj.quit()
+            sys.exit()
 
-    msg = MIMEMultipart('alternative')
-    msg['Subject'] = subject
-    msg['From'] = emailAddress
-    msg['To'] = emailAddress
+        # emailRes = smtpObj.sendmail(emailAddress, emailAddress,
+        #                             'Subject: ' + str(subject) + '\n' + str(body))
 
-    body = MIMEText(body, 'html')
-    msg.attach(body)
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = subject
+        msg['From'] = emailAddress
+        msg['To'] = emailAddress
 
-    emailRes = smtpObj.sendmail(emailAddress, emailAddress, msg.as_string())
+        body = MIMEText(body, 'html')
+        msg.attach(body)
 
-    if emailRes:
-      print('Mail delivery failed')
+        emailRes = smtpObj.sendmail(
+            emailAddress, emailAddress, msg.as_string())
 
-    smtpObj.quit()
-  else:
-    print('Error connecting to SMTP server')
-    print(connectionRes)
+        if emailRes:
+            print('Mail delivery failed')
+
+        smtpObj.quit()
+    else:
+        print('Error connecting to SMTP server')
+        print(connectionRes)
